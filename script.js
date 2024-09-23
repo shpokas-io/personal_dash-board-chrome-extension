28545df6127c825222b88be110bf2a2bf8c6b32b
shpokas <s.shpokas@gmail.com>
2024-09-23 11:39:33 +0300

    DIsplayed basic layout of where what gonna go

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ fetch(
   .then((res) => res.json())
   .then((data) => {
     document.body.style.backgroundImage = `url(${data.urls.full})`;
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
   });
