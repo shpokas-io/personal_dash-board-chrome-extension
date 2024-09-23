11c9dca00c3740180513ab2a822865228bb9a52d
shpokas <s.shpokas@gmail.com>
2024-09-23 11:24:59 +0300

    Added fetch request for background image from API

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.full})`;
+  });
