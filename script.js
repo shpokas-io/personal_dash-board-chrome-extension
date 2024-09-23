ee90640f4f963b1ed6f721f66f03d9c381f4c4de
shpokas <s.shpokas@gmail.com>
2024-09-23 12:59:58 +0300

    added weather functionality auto-locate top-right corner

--- script.js
+++ script.js
@@ -34,5 +34,22 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
   .catch((err) => console.error(err));
 
 navigator.geolocation.getCurrentPosition((position) => {
-  console.log(position);
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Weather data not available");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("weather").innerHTML = `
+      <img src=${iconUrl} />
+      <p>${Math.round(data.main.temp)} °</p>
+      <p>${data.name}</p>
+      `;
+    })
+    .catch((err) => console.error(err));
 });
